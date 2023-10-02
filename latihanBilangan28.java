2abb2383dc458f60451786cdb9d102edd91cc205
merasifa <merasifa@gmail.com>
2023-10-02 14:45:37 +0700

    latihan1

---
+++ latihanBilangan28.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class latihanBilangan28 {
+    public static void main(String[] args) {
+        Scanner input28 = new Scanner (System.in);
+        int bil1, bil2, bil3;
+
+        System.out.print("Masukkan Bilangan 1: ");
+        bil1 = input28.nextInt();
+        System.out.print("Masukkan Bilangan 2: ");
+        bil2 = input28.nextInt();
+        System.out.print("Masukkan Bilangan 3: ");
+        bil3 = input28.nextInt();
+
+        if (condition:var(boolean)) {
+            
+        } else {
+            
+        }
+    }
+}
--- suhu28.java
+++ suhu28.java
@@ -2,7 +2,27 @@ import java.util.Scanner;
 
 public class suhu28 {
     public static void main(String[] args) {
-        
+         Scanner input28 = new Scanner (System.in);
+
+         int suhu;
+         char hujan;
+
+         System.out.print("Masukkan suhu: ");
+         suhu = input28.nextInt();
+         System.out.print("Masukkan status: (hujan atau tidak)");
+         hujan = input28.next().charAt(0);
+
+         if (suhu > 27) {
+            System.out.println("Memakai Dress");
+         if (hujan == 'y' || hujan== 'Y') {
+            System.out.println("Membawa Payung");
+
+         }else {
+            System.out.println("Memakai Sunscreen");
+         }
+    }else{
+        System.out.println("Memakai Celana Panjang");
     }
     
+}
 }
