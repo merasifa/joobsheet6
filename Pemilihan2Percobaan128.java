0b0e8fd616db207ea483f514c8f2fc4f00715296
merasifa <merasifa@gmail.com>
2023-10-02 11:46:57 +0700

    penambahan 2100 menjadi bukan tahun kabisat

--- Pemilihan2Percobaan128.java
+++ Pemilihan2Percobaan128.java
@@ -9,11 +9,13 @@ public class Pemilihan2Percobaan128 {
         tahun = input28.nextInt();
 
         if ((tahun%4)== 0) {
-            if ((tahun % 100 )!= 0)
+            if ((tahun % 100 )!= 0){
             System.out.println("Tahun Kabisat");
         } else 
             System.out.println("Bukan Tahun Kabisat");
-            
+     } else {
+        System.out.println("Bukan Tahun Kabisat");
+    }
 
     }
     
---
+++ Pemilihan2Percobaan228.java
@@ -0,0 +1,10 @@
+/**
+ * Pemilihan2Percobaan228
+ */
+import java.util.Scanner;
+public class Pemilihan2Percobaan228 {
+
+    public static void main(String[] args) {
+        
+    }
+}
